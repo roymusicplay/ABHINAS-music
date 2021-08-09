78dbfcf2146f2170c6f6a69869a4d605e07750cb
ABHINAS ROY <85425565+roymusicplay@users.noreply.github.com>
2021-08-09 22:14:55 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -28,7 +28,7 @@ async def play(_, message: Message):
                 [
                     InlineKeyboardButton(
                         text="🔊 Channel",
-                        url="https://t.me/Infinity_BOTs")
+                        url="https://t.me/MOVIE_CHANNEL_1234")
                    
                 ]
             ]
@@ -59,7 +59,7 @@ async def play(_, message: Message):
     else:
         callsmusic.pytgcalls.join_group_call(message.chat.id, file_path)
         await message.reply_photo(
-        photo="https://telegra.ph/file/a4fa687ed647cfef52402.jpg",
+        photo="https://telegra.ph/file/324d9836534fcb659a442.jpg",
         reply_markup=keyboard,
         caption="▶️ **Playing** here the song requested by {}!".format(
         message.from_user.mention()
